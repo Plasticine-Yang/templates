8067ee1499b49198f0c648e217852762cf9cbf1d
plasticine <975036719@qq.com>
2022-09-21 13:40:17 +0800

    feat: integrate with eslint and prettier

---
+++ node/.eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+}
