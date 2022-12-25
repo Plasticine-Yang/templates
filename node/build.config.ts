d16f2dbe014e69296a2477f51d499523182f3716
plasticine9750 <975036719@qq.com>
2022-12-25 16:28:19 +0800

    feat(node): integrate with unbuild

---
+++ node/build.config.ts
@@ -0,0 +1,22 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+  entries: [
+    // bundle
+    './src/index',
+
+    // bundless with mkdist
+    // {
+    //   builder: 'mkdist',
+    //   input: './src/bundless/',
+    //   outDir: './dist/bundless',
+    // },
+  ],
+  outDir: 'dist',
+  clean: true,
+  declaration: true,
+
+  rollup: {
+    emitCJS: true,
+  },
+})
