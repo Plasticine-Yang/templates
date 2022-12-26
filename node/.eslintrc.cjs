2d17ac114dd841a519d5adfe19bc50880e687ce4
plasticine9750 <975036719@qq.com>
2022-12-26 11:30:36 +0800

    feat(node): use @plasticine-yang/eslint-config-typescript as eslint config

--- node/.eslintrc.cjs
+++ node/.eslintrc.cjs
@@ -1,21 +1,12 @@
 module.exports = {
+  root: true,
   env: {
     node: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  overrides: [],
-  parser: '@typescript-eslint/parser',
+  extends: ['@plasticine-yang/eslint-config-typescript'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint'],
-  rules: {
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off',
-  },
+  rules: {},
 }
